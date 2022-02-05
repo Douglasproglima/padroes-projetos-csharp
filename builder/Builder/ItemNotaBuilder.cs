bd03f05692a9269e2f2a0b01f9fe6f099a6b86e4
Douglas Lima <Douglasproglima@gmail.com>
2022-02-05 12:21:49 -0300

    Implemented ItemNFBuilder

---
+++ builder/Builder/ItemNotaBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace builder.Builder
+{
+    public class ItemNotaBuilder
+    {
+        public String Descricao { get; private set; }
+        public double Valor { get; private set; }
+
+        public ItemNota CriarItemNota(String descricao, double Valor)
+        { 
+            return new ItemNota(descricao, Valor);
+        }
+
+        public ItemNotaBuilder InserirDescricao(String descricao)
+        { 
+            this.Descricao = descricao;
+            return this;
+        }
+
+        public ItemNotaBuilder InserirValor(double valor)
+        {
+            this.Valor = valor;
+            return this;
+        }
+    }
+}
